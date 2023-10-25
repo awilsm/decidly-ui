b528fb72a47c04d8c0ad34fb546da9e37a543377
awilsm <awilsonmyers@gmail.com>
2023-10-24 19:36:58 -0700

    checkpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myApp';
+
+  pair: Pair = {} as Pair
+  decisionSessionId: string = ''
+  choiceControls: string[] = ['choice1', 'choice2'];
+
+  choiceForm = new FormGroup({
+    choice1: new FormControl(''),
+    choice2: new FormControl(''),
+  })
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.fetchPair();
+  }
+
+  fetchPair() {
+    // 5f7bb045-d6d8-46b8-9077-54bef0deff01
+    if (this.decisionSessionId !== '') {
+      this.http.get(`http://localhost:8080/decisions/${this.decisionSessionId}/pair`).subscribe(
+        (response) => {
+          this.pair = response as Pair;
+        });
+    }
+  }
+
+  addChoice() {
+    const nextChoiceNumber = this.choiceControls.length + 1;
+    const nextChoiceName = `choice${nextChoiceNumber}`;
+
+    this.choiceForm = this.formBuilder.group({
+      ...this.choiceForm.controls,
+      [nextChoiceName]: new FormControl('')
+    })
+    this.choiceControls.push(nextChoiceName);
+  }
+
+  vote(winner: string) {
+    this.http.post(`http://localhost:8080/decisions/${this.decisionSessionId}/vote`,
+      {
+        pairId: this.pair.pairId,
+        winner: winner
+      }
+    ).subscribe(
+      (response) => {
+        this.pair = {} as Pair;
+        this.fetchPair();
+      })
+  }
+
+  submit() {
+    const choices = Object.values(this.choiceForm.value).map((choice) => {
+      return { itemName: choice };
+    });
+
+    this.http.post('http://localhost:8080/choices', choices).subscribe(
+      (response) => {
+        this.decisionSessionId = response as string;
+        this.fetchPair();
+      });
+  }
+}
+
+interface CreateItemRequest {
+  itemName: string;
+}
+
+interface Item {
+  itemId: string;
+  itemName: string;
+}
+
+interface Pair {
+  pairId: string;
+  items: Item[];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TuiRootModule, TuiButtonModule } from '@taiga-ui/core';
+import {TuiBlockStatusModule} from '@taiga-ui/layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  TuiInputModule
+} from '@taiga-ui/kit';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TuiRootModule,
+    TuiButtonModule,
+    TuiBlockStatusModule,
+    TuiInputModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
